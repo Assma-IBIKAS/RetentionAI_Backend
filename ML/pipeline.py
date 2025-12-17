d418d67d1d633ff38f79707a2ca82c6c35878140
Assma-IBIKAS <Assma-IBIKAS>
2025-12-17 12:56:00 +0100

    add pipeline

---
+++ ML/pipeline.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+
+def load_data(data):
+    data = pd.read_csv(data)
+    return data
+
+def count_plot(data, column,hue='Attrition'):
+    plt.figure(figsize=(8,4))
+    sns.countplot(x=column, data=data, hue=hue,palette="pastel")
+    plt.title(f"Count Plot de {column}" + (f" par {hue}" if hue else ""))
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+def correlation(df):
+    df['Attrition'] = df['Attrition'].map({'No': 0, 'Yes': 1})
+    corr = df.corr()
+    plt.figure(figsize=(20,10))
+    sns.heatmap(corr,annot=True,cmap="Purples",fmt=".2f")
+    plt.show()
