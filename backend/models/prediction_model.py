34b29727d08cd426c71a3b3200b0609bbad67883
Assma-IBIKAS <Assma-IBIKAS>
2025-12-19 00:04:11 +0100

    initial predection model

---
+++ backend/models/prediction_model.py
@@ -0,0 +1,12 @@
+from backend.database.db import Base
+from sqlalchemy import Column, Integer, Float, DateTime, ForeignKey
+from datetime import datetime
+
+class PredictionsHistory(Base):
+    __tablename__ = "predictions_history"
+
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    employee_id = Column(Integer)
+    probability = Column(Float)
