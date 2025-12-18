da9fb7706ff7df9008425c4f9a92ba0f395a987d
Assma-IBIKAS <Assma-IBIKAS>
2025-12-19 00:04:47 +0100

    empl schema updated

---
+++ backend/schemas/employee_schema.py
@@ -0,0 +1,67 @@
+from pydantic import BaseModel
+from typing import Literal
+
+
+class employee_schema(BaseModel):
+    Age: int
+    BusinessTravel: Literal[
+        "Non-Travel",
+        "Travel_Rarely",
+        "Travel_Frequently"
+    ]
+
+    Department: Literal[
+        "Sales",
+        "Research & Development",
+        "Human Resources"
+    ]
+    Education: int
+
+    EducationField: Literal[
+        "Life Sciences",
+        "Medical",
+        "Marketing",
+        "Technical Degree",
+        "Human Resources",
+        "Other"
+    ]
+   
+    EnvironmentSatisfaction: int
+
+    Gender: Literal["Male", "Female"]
+
+    JobInvolvement: int
+    JobLevel: int
+
+    JobRole: Literal[
+        "Sales Executive",
+        "Research Scientist",
+        "Laboratory Technician",
+        "Manufacturing Director",
+        "Healthcare Representative",
+        "Manager",
+        "Sales Representative",
+        "Research Director",
+        "Human Resources"
+    ]
+
+    JobSatisfaction: int
+
+    MaritalStatus: Literal[
+        "Single",
+        "Married",
+        "Divorced"
+    ]
+
+    MonthlyIncome: int
+
+    OverTime: Literal["Yes", "No"]
+
+    PerformanceRating: int
+    RelationshipSatisfaction: int
+    StockOptionLevel: int
+    TotalWorkingYears: int
+    WorkLifeBalance: int
+    YearsAtCompany: int
+    YearsInCurrentRole: int
+    YearsWithCurrManager: int
