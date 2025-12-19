8bdc88270c1f3750d8d109119bd66c914a7450fb
Assma-IBIKAS <Assma-IBIKAS>
2025-12-19 15:39:30 +0100

    security changes

--- backend/services/security.py
+++ backend/services/security.py
@@ -1,6 +1,6 @@
-from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt
+import bcrypt
 from dotenv import load_dotenv
 load_dotenv()
 import os
@@ -10,13 +10,20 @@ SECRET_KEY = os.getenv('SC')
 ALGORITHM = os.getenv('ALGORITHM')
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str):
-    return pwd_context.hash(password)
+#fonction pour hasher le mot de passe avec bcrypt
+def hash_password(password: str) -> str:
+    salt = bcrypt.gensalt()                 # génère un salt
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed.decode('utf-8')           # convertir bytes → string pour stockage
 
-def verify_password(password, hashed):
-    return pwd_context.verify(password, hashed)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    # Tronquer et encoder le mot de passe en clair
+    password_bytes = plain_password.encode("utf-8")[:72]
+    # Comparer avec le hash existant
+    return bcrypt.checkpw(password_bytes, hashed_password.encode("utf-8"))
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
