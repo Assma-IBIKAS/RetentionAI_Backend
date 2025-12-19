aca0e496ae0f39a56037363ad59d21ddeba30dac
Assma-IBIKAS <Assma-IBIKAS>
2025-12-19 15:38:17 +0100

    user model changes

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,10 +1,12 @@
 from backend.database.db import Base
 from sqlalchemy import Column, DateTime, Integer, String
-from datetime import datetime
+from datetime import datetime, timezone
 
 class users(Base):
     __tablename__= "users"
-    id= Column(Integer,primary_key=True)
-    username= Column(String, nullable=False)
-    passwordhash = Column(String, nullable=False)
-    created_at= Column(DateTime, default= datetime.now )
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    password_hash = Column(String)
+    created_at = Column(
+        DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
+    )
