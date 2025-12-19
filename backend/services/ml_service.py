96cbffd0cb53686c62b2de416e99a1eda1170b4e
Assma-IBIKAS <Assma-IBIKAS>
2025-12-19 15:39:19 +0100

    ml service changes

--- backend/services/ml_service.py
+++ backend/services/ml_service.py
@@ -4,11 +4,11 @@ import joblib
 model = joblib.load("ML/lr_model.pkl")
 
 
-def predict_churn(data):
+def predict_churn(df):
     """
     data : liste ou array des features déjà préparées
     """
     # Si model est un pipeline, il gère la transformation automatiquement
     # Si model est juste le modèle, data doit être déjà transformé
-    proba = model.predict_proba([data])[0][1]
+    proba = model.predict_proba(df)[0][1]
     return float(proba)
